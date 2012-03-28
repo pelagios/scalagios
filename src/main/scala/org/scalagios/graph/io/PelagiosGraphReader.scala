f0b3d6876ef4452e5a3cc4f063ea24b19bb4bbe8
Rainer Simon <rainer.simon@ait.ac.at>
2012-03-28 09:34:09 +0200

    Read access to Places and Datasets (without pagination)

--- src/main/scala/org/scalagios/graph/io/PelagiosGraphReader.scala
+++ src/main/scala/org/scalagios/graph/io/PelagiosGraphReader.scala
@@ -1,9 +1,11 @@
 package org.scalagios.graph.io
 
+import scala.collection.JavaConverters._
 import com.tinkerpop.blueprints.pgm.IndexableGraph
 import org.scalagios.graph.Constants._
 import com.tinkerpop.blueprints.pgm.{Vertex, IndexableGraph}
 import org.scalagios.api.{Place, Dataset}
+import com.tinkerpop.frames.FramesManager
 
 /**
  * Provides Pelagios-specific Graph DB I/O (read) features.
@@ -11,13 +13,13 @@ import org.scalagios.api.{Place, Dataset}
  * @author Rainer Simon <rainer.simon@ait.ac.at>
  */
 class PelagiosGraphReader[T <: IndexableGraph](graph: T) extends PelagiosGraphIOBase(graph) {
-
-  def getPlaces(): List[Place] = {
-    List[Place]()
-  }
   
-  def getDatasets(): List[Dataset] = {
-    List[Dataset]()
-  }
+  private val framesManager: FramesManager = new FramesManager(graph)
+
+  def getPlaces(): Iterable[Place] = getVertices(PLACE_VERTEX).map(vertex => framesManager.frame(vertex, classOf[Place]))
+  
+  def getDatasets(): Iterable[Dataset] = getVertices(DATASET_VERTEX).map(vertex => framesManager.frame(vertex, classOf[Dataset]))
+  
+  private def getVertices(vertexType: String) = graph.getVertices().asScala.filter(_.getProperty(VERTEX_TYPE).equals(vertexType))
   
 }
