3974f202ee5c05698a95441b995e4a4de676cf7c
Rainer Simon <rainer.simon@ait.ac.at>
2012-04-12 16:07:20 +0200

    'source' property added to DCTerms vocab impl

--- src/main/scala/org/scalagios/rdf/vocab/DCTerms.scala
+++ src/main/scala/org/scalagios/rdf/vocab/DCTerms.scala
@@ -3,13 +3,15 @@ package org.scalagios.rdf.vocab
 object DCTerms extends BaseVocab {
   
   val NAMESPACE = "http://purl.org/dc/terms/"
-    
-  val title = factory.createURI(NAMESPACE, "title")
+
+  val coverage = factory.createURI(NAMESPACE, "coverage")
 
   val description = factory.createURI(NAMESPACE, "description")
   
   val license = factory.createURI(NAMESPACE, "license")
   
-  val coverage = factory.createURI(NAMESPACE, "coverage")
+  val source = factory.createURI(NAMESPACE, "source")
+  
+  val title = factory.createURI(NAMESPACE, "title")
 
 }
