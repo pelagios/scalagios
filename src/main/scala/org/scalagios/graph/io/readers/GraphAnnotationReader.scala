6e60cd17514c3aa1235ba961725963238aa4a2a2
Rainer Simon <rainer.simon@ait.ac.at>
2012-06-27 17:47:39 +0200

    Graph reader feature: get parent dataset for annotation

--- src/main/scala/org/scalagios/graph/io/readers/GraphAnnotationReader.scala
+++ src/main/scala/org/scalagios/graph/io/readers/GraphAnnotationReader.scala
@@ -1,10 +1,10 @@
 package org.scalagios.graph.io.readers
 
 import org.scalagios.graph.io.PelagiosGraphIOBase
-import org.scalagios.api.{GeoAnnotation, Place}
+import org.scalagios.api.{Dataset, GeoAnnotation, Place}
 import org.scalagios.graph.Constants._
 import org.scalagios.graph.VertexExtensions._
-import org.scalagios.graph.{GeoAnnotationVertex, PlaceVertex}
+import org.scalagios.graph.{DatasetVertex, GeoAnnotationVertex, PlaceVertex}
 import org.scalagios.graph.exception.GraphIntegrityException
 
 trait GraphAnnotationReader extends PelagiosGraphIOBase {
@@ -15,6 +15,14 @@ trait GraphAnnotationReader extends PelagiosGraphIOBase {
       new PlaceVertex(place.get)
     else
       throw new GraphIntegrityException("Graph corrupt: annotation " + annotation.uri + " disconnected from body")
+  }
+  
+  def getParentDataset(annotation: GeoAnnotation): Dataset =  {
+    val dataset = annotation.asInstanceOf[GeoAnnotationVertex].vertex.getInNeighbour(RELATION_CONTAINS)
+    if (dataset.isDefined)
+      new DatasetVertex(dataset.get)
+    else
+      throw new GraphIntegrityException("Graph corrupt: annotation " + annotation.uri + " disconnected from dataset")
   }
   
 }
