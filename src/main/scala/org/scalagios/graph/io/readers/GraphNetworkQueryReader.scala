6ccdc3723dfedea24507b203e5dacc7870a90c61
Rainer Simon <rainer.simon@ait.ac.at>
2012-06-27 18:09:33 +0200

    Started network query implementation

---
+++ src/main/scala/org/scalagios/graph/io/readers/GraphNetworkQueryReader.scala
@@ -0,0 +1,9 @@
+package org.scalagios.graph.io.readers
+
+import org.scalagios.graph.io.PelagiosGraphIOBase
+
+trait GraphNetworkQueryReader extends PelagiosGraphIOBase {
+
+  // TODO implement
+  
+}
