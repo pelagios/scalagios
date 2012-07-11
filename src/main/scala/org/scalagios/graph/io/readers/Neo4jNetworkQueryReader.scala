f26a38c05f1a720874397889c2b3f31e323d16fc
Rainer Simon <rainer.simon@ait.ac.at>
2012-07-11 13:05:25 +0200

    Wrapped Neo4j paths for more convenience in the API

--- src/main/scala/org/scalagios/graph/io/readers/Neo4jNetworkQueryReader.scala
+++ src/main/scala/org/scalagios/graph/io/readers/Neo4jNetworkQueryReader.scala
@@ -7,8 +7,13 @@ import org.scalagios.graph.io.PelagiosGraphIOBase
 import org.scalagios.graph.exception.GraphIOException
 import org.neo4j.kernel.Traversal
 import org.neo4j.graphalgo.GraphAlgoFactory
-import org.neo4j.graphdb.{Direction, DynamicRelationshipType, Node, Path}
+import org.neo4j.graphdb.{Direction, DynamicRelationshipType, Node}
+import org.neo4j.graphdb.{Path => Neo4jPath}
 import com.tinkerpop.blueprints.pgm.impls.neo4j.Neo4jVertex
+import com.tinkerpop.blueprints.pgm.impls.neo4j.Neo4jGraph
+import org.scalagios.graph.PlaceVertex
+import org.scalagios.graph.DatasetVertex
+import org.scalagios.graph.exception.GraphIntegrityException
 
 trait Neo4jNetworkQueryReader extends PelagiosGraphIOBase {
 
@@ -37,7 +42,30 @@ trait Neo4jNetworkQueryReader extends PelagiosGraphIOBase {
     val to = _getPlaceNodeOrThrowException(toUri)
  
     val finder = GraphAlgoFactory.shortestPath(pathExpander, 5);
-    finder.findAllPaths(from, to).asScala
+    finder.findAllPaths(from, to).asScala.map(new Path(_, graph.asInstanceOf[Neo4jGraph]))
+  }
+  
+}
+
+class Path(neo4jPath: Neo4jPath, graph: Neo4jGraph) {
+  
+  val MSG_INTEGRITY_EXCEPTION = "Something else than place or dataset found during path search: "
+  
+  lazy val startPlace = new PlaceVertex(new Neo4jVertex(neo4jPath.startNode, graph))
+  
+  lazy val endPlace = new PlaceVertex(new Neo4jVertex(neo4jPath.endNode, graph))
+  
+  lazy val length = neo4jPath.length
+  
+  lazy val nodes = {
+    neo4jPath.nodes.asScala.map(node => {
+      new Neo4jVertex(node, graph) match {
+        case v: Neo4jVertex if v.getProperty(VERTEX_TYPE).equals(PLACE_VERTEX) => new PlaceVertex(v)
+        case v: Neo4jVertex if v.getProperty(VERTEX_TYPE).equals(DATASET_VERTEX) => new DatasetVertex(v)
+        // Should never happen
+        case v: Neo4jVertex => throw GraphIntegrityException(MSG_INTEGRITY_EXCEPTION + v.getProperty(VERTEX_TYPE))
+      }
+    })
   }
   
 }
