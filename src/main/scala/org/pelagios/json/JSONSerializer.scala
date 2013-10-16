bb1423c6280bbe9fe01558c1be302f3c462f3965
Rainer Simon <rainer.simon@ait.ac.at>
2013-10-16 12:45:58 +0200

    JSON serialization for layout paths

--- src/main/scala/org/pelagios/json/JSONSerializer.scala
+++ src/main/scala/org/pelagios/json/JSONSerializer.scala
@@ -2,6 +2,8 @@ package org.pelagios.json
 
 import org.pelagios.api._
 import net.liftweb.json._
+import org.pelagios.api.layout.Link
+import com.vividsolutions.jts.geom.Coordinate
 
 object JSONSerializer {
   
@@ -26,20 +28,6 @@ object JSONSerializer {
     ("geometry" -> locations) 
   }
   
-  def serializePlaces(places: Seq[Place], prettyPrint: Boolean = false): String = {
-    val json = places.map(placeToJSON(_)) 
-    if (prettyPrint) pretty(render(json)) else compact(render(json))
-  }
-  
-  def serializeAnnotations(annotations: Seq[Annotation]): String = 
-    _serializeAnnotations(annotations, false, None)
-  
-  def serializeAnnotations(annotations: Seq[Annotation], pretty: Boolean): String = 
-    _serializeAnnotations(annotations, pretty, None)
-  
-  def serializeAnnotations(annotations: Seq[Annotation], pretty: Boolean, geoResolutionFn: String => Option[Place]): String = 
-    _serializeAnnotations(annotations, pretty, Some(geoResolutionFn))
-    
   private def _serializeAnnotations(annotations: Seq[Annotation], 
                                    prettyPrint: Boolean = false, 
                                    geoResolutionFn: Option[String => Option[Place]] = None): String = {
@@ -59,6 +47,57 @@ object JSONSerializer {
       ("place" -> placeToJSON(place))
     }}
       
+    if (prettyPrint) pretty(render(json)) else compact(render(json))
+  }
+  
+  def serializePlaces(places: Seq[Place], prettyPrint: Boolean = false): String = {
+    val json = places.map(placeToJSON(_)) 
+    if (prettyPrint) pretty(render(json)) else compact(render(json))
+  }
+  
+  def serializeAnnotations(annotations: Seq[Annotation]): String = 
+    _serializeAnnotations(annotations, false, None)
+  
+  def serializeAnnotations(annotations: Seq[Annotation], pretty: Boolean): String = 
+    _serializeAnnotations(annotations, pretty, None)
+  
+  def serializeAnnotations(annotations: Seq[Annotation], pretty: Boolean, geoResolutionFn: String => Option[Place]): String = 
+    _serializeAnnotations(annotations, pretty, Some(geoResolutionFn))
+    
+
+  def serializeLinks(links: Seq[Link], prettyPrint: Boolean, geoResolutionFn: String => Option[Place]): String = {
+    val lines = links.foldLeft(Seq.empty[(Coordinate, Coordinate)])((result, current) => {
+      if (current.from.place.size > 0 && current.to.place.size > 0) {
+        val from = geoResolutionFn(current.from.place(0))
+        val to = geoResolutionFn(current.to.place(0))
+      
+        if (from.isDefined && to.isDefined) {
+          val fromLocations = from.get.locations
+          val toLocations = to.get.locations
+        
+          if (fromLocations.size > 0 && toLocations.size > 0) {
+            val fromCoord = fromLocations(0).geometry.getCentroid.getCoordinate
+            val toCoord = toLocations(0).geometry.getCentroid.getCoordinate
+            (fromCoord, toCoord) +: result
+          } else {
+            result  
+          }
+        } else {
+          result      
+        }
+      } else {
+        result
+      }
+    })
+    
+    val json = lines.map { case (from, to) => {
+      ("type" -> "Feature") ~ 
+      ("geometry" -> 
+          ("type" -> "LineString") ~ 
+          ("coordinates" -> Seq(Seq(from.x, from.y), Seq(to.x, to.y)))
+      )
+    }}
+    
     if (prettyPrint) pretty(render(json)) else compact(render(json))
   }
 
