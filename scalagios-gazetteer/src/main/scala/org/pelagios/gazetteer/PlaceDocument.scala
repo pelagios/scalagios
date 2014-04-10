2ab2cae9de52b224c361e3fa1d1d187f3336c199
Rainer Simon <rainer.simon@ait.ac.at>
2014-04-10 17:18:38 +0200

    Changed lucene geometry format from WKT to GeoJSON

--- scalagios-gazetteer/src/main/scala/org/pelagios/gazetteer/PlaceDocument.scala
+++ scalagios-gazetteer/src/main/scala/org/pelagios/gazetteer/PlaceDocument.scala
@@ -5,6 +5,7 @@ import org.apache.lucene.index.IndexableField
 import org.pelagios.api.{ Label, Location, Place, PlaceCategory }
 import scala.collection.JavaConversions._
 import com.vividsolutions.jts.io.WKTWriter
+import org.geotools.geojson.geom.GeometryJSON
 
 /** An implementation of the [[Place]] API primitive backed by a Lucene Document.
   *  
@@ -22,7 +23,7 @@ class PlaceDocument private[gazetteer] (doc: Document) extends Place {
   val names: Seq[Label] = 
     doc.getFields().filter(_.name.startsWith(PlaceIndex.FIELD_NAME)).map(field => toLabel(field))
   
-  val locations: Seq[Location] = doc.getValues(PlaceIndex.FIELD_GEOMETRY).map(wkt => Location(Location.parseWKT(wkt))).toSeq
+  val locations: Seq[Location] = doc.getValues(PlaceIndex.FIELD_GEOMETRY).map(json => Location(Location.parseGeoJSON(json))).toSeq
   
   val category: Option[PlaceCategory.Category] = toOption(doc.get(PlaceIndex.FIELD_CATEGORY)).map(PlaceCategory.withName(_))
   
@@ -47,8 +48,6 @@ class PlaceDocument private[gazetteer] (doc: Document) extends Place {
 object PlaceDocument {
   
   def apply(place: Place, seedURI: Option[String]) = {
-    val wktWriter = new WKTWriter()
-    
     val doc = new Document()
     doc.add(new StringField(PlaceIndex.FIELD_URI, GazetteerUtils.normalizeURI(place.uri), Field.Store.YES))
     doc.add(new TextField(PlaceIndex.FIELD_TITLE, place.title, Field.Store.YES))
@@ -59,7 +58,7 @@ object PlaceDocument {
     place.names.foreach(name => {
       doc.add(new TextField(PlaceIndex.FIELD_NAME, name.label, Field.Store.YES)) 
     })
-    place.locations.foreach(location => doc.add(new StringField(PlaceIndex.FIELD_GEOMETRY, wktWriter.write(location.geometry), Field.Store.YES)))
+    place.locations.foreach(location => doc.add(new StringField(PlaceIndex.FIELD_GEOMETRY, location.geoJSON, Field.Store.YES)))
     if (place.category.isDefined)
       doc.add(new StringField(PlaceIndex.FIELD_CATEGORY, place.category.get.toString, Field.Store.YES))
     place.subjects.foreach(subject => doc.add(new StringField(PlaceIndex.FIELD_SUBJECT, subject, Field.Store.YES)))
