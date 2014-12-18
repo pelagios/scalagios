1c7cb35f03dd7e654c0aacf35e8ab80dbe72ea18
Rainer Simon <rainer.simon@ait.ac.at>
2014-12-18 16:55:17 +0000

    Minor edits

--- scalagios-core/src/test/scala/org/pelagios/rdf/parser/StreamingGazetteerParserTest.scala
+++ scalagios-core/src/test/scala/org/pelagios/rdf/parser/StreamingGazetteerParserTest.scala
@@ -7,19 +7,24 @@ import java.io.FileInputStream
 import org.pelagios.api.gazetteer.Place
 import org.pelagios.Scalagios
 import org.openrdf.rio.RDFFormat
+import java.util.zip.GZIPInputStream
 
 
 @RunWith(classOf[JUnitRunner])
 class StreamingGazetteerParserTest extends FunSuite with UsesTestData {
 
   private val TEST_FILE = getFile("test-places-pleiades.ttl")
+  private val DATA_DARE = "../test-data/dare-20140324.ttl.gz"
   // private val TEST_FILE = getFile("wikidata.ttl")
   
   test("Streaming Gazetteer Dump Import") {
-    val is = new FileInputStream(TEST_FILE)
+    val is = new GZIPInputStream(new FileInputStream(DATA_DARE))
         
     def streamHandler(place: Place): Unit = {
-      println(place.label)
+      // println(place.label)
+      val languages = place.names.flatMap(_.lang).distinct
+      if (languages.size > 0)
+        println(languages.mkString(", "))
     }
     
     Scalagios.streamPlaces(is, TEST_FILE.getName, streamHandler)
