efc2faae518fa1880f8775b7eecfc37d59666a16
Rainer Simon <rainer.simon@ait.ac.at>
2015-11-12 08:56:27 +0100

    Catching gazetteer RDF parse errors on per-place basis & writing error log output

--- scalagios-core/src/main/scala/org/pelagios/rdf/parser/gazetteer/PlaceStreamHandler.scala
+++ scalagios-core/src/main/scala/org/pelagios/rdf/parser/gazetteer/PlaceStreamHandler.scala
@@ -18,40 +18,47 @@ class PlaceStreamHandler(val onNextPlace: Place => Unit, lowMemoryMode: Boolean)
     })
     
     placeResources.foreach(resource => {
-      val nameURIs = resource.get(LAWD.hasName)
-      val locationURI = resource.getFirst(W3CGeo.location)
-      val geometryURI = resource.getFirst(GeoSPARQL.hasGeometry)
-      val imageURIs = resource.get(FOAF.depiction)
+      try {
+        val nameURIs = resource.get(LAWD.hasName)
+        val locationURI = resource.getFirst(W3CGeo.location)
+        val geometryURI = resource.getFirst(GeoSPARQL.hasGeometry)
+        val imageURIs = resource.get(FOAF.depiction)
       
-	    val nameResources = nameURIs.flatMap(uri => cache.get(uri.stringValue))
-      val locationResource = locationURI.flatMap(uri => cache.get(uri.stringValue))
-      val geometryResource = geometryURI.flatMap(uri => cache.get(uri.stringValue))
-      val imageResources = imageURIs.flatMap(uri => cache.get(uri.stringValue))
+	      val nameResources = nameURIs.flatMap(uri => cache.get(uri.stringValue))
+        val locationResource = locationURI.flatMap(uri => cache.get(uri.stringValue))
+        val geometryResource = geometryURI.flatMap(uri => cache.get(uri.stringValue))
+        val imageResources = imageURIs.flatMap(uri => cache.get(uri.stringValue))
 
-      // If all name, image, location, geometry URIs in the are in the cache, we consider it complete 
-      val isComplete = 
-        ((nameResources.size == nameURIs.size) && (imageResources.size == imageURIs.size) &&
-         (locationResource.size == locationURI.size) && (geometryResource.size == geometryURI.size))
+        // If all name, image, location, geometry URIs in the are in the cache, we consider it complete 
+        val isComplete = 
+          ((nameResources.size == nameURIs.size) && (imageResources.size == imageURIs.size) &&
+           (locationResource.size == locationURI.size) && (geometryResource.size == geometryURI.size))
          
-      if (isComplete) {
-		    // We notify the callback handler...
-        val names = nameResources.map(_.getFirst(LAWD.primaryForm).map(ResourceCollector.toPlainLiteral(_)).get)
-        val coordinate = locationResource.flatMap(PlaceResource.toCoordinate(_))
-        val geometry = geometryResource.flatMap(PlaceResource.toGeometry(_))
-        val images = imageResources.map(PlaceResource.toImage(_))
+        if (isComplete) {
+		      // We notify the callback handler...
+          val names = nameResources.map(_.getFirst(LAWD.primaryForm).map(ResourceCollector.toPlainLiteral(_)).get)
+          val coordinate = locationResource.flatMap(PlaceResource.toCoordinate(_))
+          val geometry = geometryResource.flatMap(PlaceResource.toGeometry(_))
+          val images = imageResources.map(PlaceResource.toImage(_))
         
-        val placeResource = new PlaceResource(resource, names, Location.create(coordinate, geometry), images)
-        onNextPlace(placeResource)
+          val placeResource = new PlaceResource(resource, names, Location.create(coordinate, geometry), images)
+          onNextPlace(placeResource)
         
-        // ...clear the place (along with all linked resources) from the cache...
-        cache.remove(resource.uri)
-        nameURIs.foreach(uri => cache.remove(uri.stringValue))
-        locationURI.map(uri => cache.remove(uri.stringValue))
-        geometryURI.map(uri => cache.remove(uri.stringValue))
-        imageURIs.foreach(uri => cache.remove(uri.stringValue))
+          // ...clear the place (along with all linked resources) from the cache...
+          cache.remove(resource.uri)
+          nameURIs.foreach(uri => cache.remove(uri.stringValue))
+          locationURI.map(uri => cache.remove(uri.stringValue))
+          geometryURI.map(uri => cache.remove(uri.stringValue))
+          imageURIs.foreach(uri => cache.remove(uri.stringValue))
         
-        // ...and increment the counter
-        placeCounter += 1
+          // ...and increment the counter
+          placeCounter += 1
+        }
+      } catch {
+        case t: Throwable => { 
+          logger.warn(resource.uri)
+          logger.warn(t.getMessage)  
+        }
       }
 	})
 	
