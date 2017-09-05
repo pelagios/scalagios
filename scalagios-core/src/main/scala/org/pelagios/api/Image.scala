085bdc96653a7c40e86bf64ad3bac2fba34a3e50
Rainer Simon <rainer.simon@ait.ac.at>
2017-09-05 11:37:17 +0200

    Added IIIF endpoint field to Image domain trait

--- scalagios-core/src/main/scala/org/pelagios/api/Image.scala
+++ scalagios-core/src/main/scala/org/pelagios/api/Image.scala
@@ -8,13 +8,21 @@ trait Image {
   
   def license: Option[String]
   
+  def iiifEndpoint: Option[String]
+  
 }
 
-private[api] class DefaultImage(val uri: String, val title: Option[String], val license: Option[String]) extends Image
+private[api] class DefaultImage(val uri: String, val title: Option[String], val license: Option[String], val iiifEndpoint: Option[String]) extends Image
 
 object Image {
   
-  def apply(uri: String, title: Option[String] = None, license: Option[String] = None)
-    = new DefaultImage(uri, None, None)
+  def apply(uri: String, title: Option[String] = None, license: Option[String] = None, iiifUrl: Option[String] = None) = {
+    val validIiifUrl = iiifUrl match {
+      case Some(url) if url.endsWith("info.json") => Some(url)
+      case _ => None
+    }
+      
+    new DefaultImage(uri, None, None, validIiifUrl)
+  }
   
 }
